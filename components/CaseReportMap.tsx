a54faed867ab4eff349fa82f3bf4403a891d1081
Brian P. Hamachek <brian@brianhama.com>
2025-03-26 22:03:40 -0700

    Update CaseReportMap.tsx

--- components/CaseReportMap.tsx
+++ components/CaseReportMap.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { APIProvider, Map } from '@vis.gl/react-google-maps';
+import { APIProvider, ColorScheme, Map } from '@vis.gl/react-google-maps';
 import { ArrestReport } from './ArrestReport';
 import { ClusteredArrestMarkers } from './ClusteredArrestMarker';
 
@@ -15,88 +15,9 @@ export default function CaseReportMap({reports, setSelectedReportId}: {reports: 
     mapId="4712fd750ea17be4"
       defaultZoom={13}
       gestureHandling={'greedy'}
+      colorScheme={ColorScheme.DARK}
       disableDefaultUI={true}
-      defaultCenter={{ lat: 37.4419, lng: -122.1430 }}><ClusteredArrestMarkers arrests={reports} setSelectedReportId={setSelectedReportId}
-      styles={[
-        { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-        { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
-        { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
-        {
-          featureType: "administrative.locality",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#d59563" }],
-        },
-        {
-          featureType: "poi",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#d59563" }],
-        },
-        {
-          featureType: "poi.park",
-          elementType: "geometry",
-          stylers: [{ color: "#263c3f" }],
-        },
-        {
-          featureType: "poi.park",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#6b9a76" }],
-        },
-        {
-          featureType: "road",
-          elementType: "geometry",
-          stylers: [{ color: "#38414e" }],
-        },
-        {
-          featureType: "road",
-          elementType: "geometry.stroke",
-          stylers: [{ color: "#212a37" }],
-        },
-        {
-          featureType: "road",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#9ca5b3" }],
-        },
-        {
-          featureType: "road.highway",
-          elementType: "geometry",
-          stylers: [{ color: "#746855" }],
-        },
-        {
-          featureType: "road.highway",
-          elementType: "geometry.stroke",
-          stylers: [{ color: "#1f2835" }],
-        },
-        {
-          featureType: "road.highway",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#f3d19c" }],
-        },
-        {
-          featureType: "transit",
-          elementType: "geometry",
-          stylers: [{ color: "#2f3948" }],
-        },
-        {
-          featureType: "transit.station",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#d59563" }],
-        },
-        {
-          featureType: "water",
-          elementType: "geometry",
-          stylers: [{ color: "#17263c" }],
-        },
-        {
-          featureType: "water",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#515c6d" }],
-        },
-        {
-          featureType: "water",
-          elementType: "labels.text.stroke",
-          stylers: [{ color: "#17263c" }],
-        }]
-      }/></Map>
+      defaultCenter={{ lat: 37.4419, lng: -122.1430 }}><ClusteredArrestMarkers arrests={reports} setSelectedReportId={setSelectedReportId} /> </Map>
   </APIProvider></div>
     );
 }
